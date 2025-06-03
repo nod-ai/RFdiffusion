ad2a8f2ca3d3458e162e7e6c6c9fe67440a1a447
Geoffrey Martin-Noble <gcmn@brium.ai>
2025-06-02 19:23:51 -0700

    Report RMSD even from passing tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,58 @@
+import statistics
+
 import pytest
 
 def pytest_addoption(parser):
     parser.addoption("--update-goldens", action="store_true", help="Update golden reference output files")
+
+
+def pytest_terminal_summary(terminalreporter, exitstatus, config):
+    """Custom terminal summary to display RMSD values and reference file creation."""
+    tr = terminalreporter
+    rmsds = {}
+    reference_files = {}
+
+    for report in tr.getreports(""):
+        for name, value in getattr(report, "user_properties", []):
+            test_name = report.nodeid
+            if name == "rmsd":
+                rmsds[test_name] = value
+            elif name == "reference_file":
+                reference_files[test_name] = value
+
+    if rmsds:
+        tr.write_sep("=", "RMSD Report")
+        if len(rmsds) > 1:
+            value_sorted_rmsds = sorted(rmsds.items(), key=lambda x: x[1])
+            min_rmsd = value_sorted_rmsds[0]
+            max_rmsd = value_sorted_rmsds[-1]
+
+            rmsd_values = list(rmsds.values())
+            mean_rmsd = statistics.fmean(rmsd_values)
+            std_rmsd = statistics.stdev(rmsd_values)
+
+            tr.write_line(f"mean:   {mean_rmsd:.3}")
+            tr.write_line(f"stddev: {std_rmsd:.3}")
+            tr.write_line(f"max:    {max_rmsd[1]:.3}")
+            tr.write_line(f"min:    {min_rmsd[1]:.3}")
+
+            tr.write_line(f"Max RMSD from: {max_rmsd[0]}")
+            tr.write_line(f"Min RMSD from: {min_rmsd[0]}")
+
+            tr.write_line("")
+
+        tr.write_line("RMSD for all tests:")
+
+        for test_name, rmsd in sorted(rmsds.items()):
+            # Make all the numbers aligned for ease of reading
+            tr.write_line(f"{rmsd:6.3}  {test_name}")
+        tr.write_line("")
+
+    if reference_files:
+        tr.write_sep("=", "Reference Files Report")
+        tr.write_line("Reference files written to...")
+        for test_name, ref_file in sorted(reference_files.items()):
+            tr.write_line(f"{test_name}")
+            # filepaths tend to be long and wrap poorly. Give them their own line.
+            tr.write_line(f"    {ref_file}")
+        tr.write_line("")
--- tests/test_diffusion.py
+++ tests/test_diffusion.py
@@ -1,4 +1,3 @@
-import datetime
 import pathlib
 import shutil
 import subprocess
@@ -10,15 +9,6 @@ from rfdiffusion.util import calc_rmsd
 
 script_dir = pathlib.Path(__file__).parent
 example_dir = script_dir.parent / "examples"
-
-
-@pytest.fixture(scope="module")
-def output_dir():
-    now = datetime.datetime.now()
-    now = now.strftime("%Y_%m_%d_%H_%M_%S")
-    d = script_dir / f"tests_{now}"
-    d.mkdir()
-    return d
 
 
 @pytest.fixture(scope="module")
@@ -39,31 +29,45 @@ def symlink_inputs():
             link.symlink_to(p)
 
 
-@pytest.fixture(
-    scope="module", params=sorted(example_dir.glob("*.sh")), ids=lambda x: x.stem
-)
-def modified_script(request, output_dir):
-    return _write_command(request.param, output_dir)
-
-
-def test_command(modified_script, output_dir, reference_dir, symlink_inputs, request):
+@pytest.mark.usefixtures("symlink_inputs")
+@pytest.mark.parametrize("script", sorted(example_dir.glob("*.sh")), ids=lambda x: x.stem)
+def test_command(script, tmp_path, reference_dir, request):
+    output_dir = tmp_path
+    # The pytest docs say you need to create this directory, but empirically it
+    # is already created.
+    # output_dir.mkdir()
+    modified_script = _write_command(script, output_dir)
     print(f"Running {modified_script}")
-    subprocess.run(["bash", modified_script], check=True)
+    # cwd is required because the scripts use relative paths like `../scripts/run_inference.py`
+    subprocess.run(["bash", modified_script], check=True, cwd=script_dir)
     test_name = modified_script.stem
-    test_files = list((output_dir / "example_outputs" / test_name).glob("*.pdb"))
-    assert len(test_files) == 1
-    test_file = test_files[0]
-    test_pdb = iu.parse_pdb(test_file)
+    test_file = output_dir / f"{test_name}_0.pdb"
     reference_file = reference_dir / test_file.relative_to(output_dir)
+
+    # We store the reference path file or the RMSD value on the test node for
+    # later reporting using user_properties. See `pytest_terminal_summary` in
+    # conftest.py for how these are used. I'm not sure whether this it the best
+    # way to do this. I didn't find a clear blessed way to have tests report
+    # additional non-status outputs. Anything in these properties needs to be
+    # serializable by execnet in order to use pytest-xdist (which basically
+    # means only Python primitives).
+
     if request.config.getoption("--update-goldens"):
         reference_file.parent.mkdir(parents=True, exist_ok=True)
         shutil.copyfile(test_file, reference_file)
+        # Anything in these properties needs to be serializable by execnet in
+        # order to use pytest-xdist, hence converting to a string.
+        request.node.user_properties.append(("reference_file", str(reference_file)))
         print(f"Updated reference file {reference_file}")
     else:
+        test_pdb = iu.parse_pdb(test_file)
         ref_pdb = iu.parse_pdb(reference_file)
         rmsd = calc_rmsd(
             test_pdb["xyz"][:, :3].reshape(-1, 3), ref_pdb["xyz"][:, :3].reshape(-1, 3)
-        )[0]
+        )[0].item()
+        request.node.user_properties.append(("rmsd", rmsd))
+        print(f"RMSD={rmsd:.3}")
+
         assert rmsd == pytest.approx(0, rel=0, abs=0.01)
 
 
@@ -95,7 +99,7 @@ def _write_command(bash_file, output_dir):
                 command = (
                     f"{command}"
                     f" inference.num_designs=1"
-                    f" inference.output_prefix={output_dir}/example_outputs/{test_name}/{test_name}"
+                    f" inference.output_prefix={output_dir}/{test_name}"
                     f" inference.deterministic=True"
                     f" inference.final_step={final_step}"
                 )
